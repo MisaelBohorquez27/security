739631b32ab231a93984d735c337c32eeda82792
MisaelBohorquez27 <67847941+MisaelBohorquez27@users.noreply.github.com>
2024-07-30 20:53:06 -0500

    Terminando y arreglando la guia 21

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const session = require('express-session');
 
 /* 1. Referencia a los middlewares */
 var authenticateSession = require('./middleware/authentication_session');
+var authorizationSession = require('./middleware/authorization_session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -41,7 +42,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', authenticateSession, usersRouter);
+app.use('/users', authenticateSession, authorizationSession, usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
