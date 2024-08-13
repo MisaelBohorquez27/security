f2c6d070c40589f1c27190f2d3128034a2c3f622
IsmaelYumi <ismeyumi@espol.edu.ec>
2024-08-13 00:49:39 -0500

    Alterado pero sirve

--- middleware/authorization_session.js
+++ middleware/authorization_session.js
@@ -2,10 +2,18 @@
 
  var authorizationSession = (req, res, next) => {
     if(process.env.ALL_GRANTED.includes(req.session.role)) {
-        return next()
+        return  res.next();
+    } else{
+        return res.redirect("/")
+    }
+}
+var  UserSession = (req, res, next) => {
+    if(process.env.USER_GRANTED.includes(req.session.role)) {
+        return res.next;
     } else{
         return res.redirect("/")
     }
 }
 
 module.exports = authorizationSession;
+module.exports=UserSession;
