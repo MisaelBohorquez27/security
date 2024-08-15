5cb377adee7cfc55b19b89e22cb9aecd9e4e6efb
IsmaelYumi <ismeyumi@espol.edu.ec>
2024-08-14 20:53:48 -0500

    Generacion de token

--- routes/token.js
+++ routes/token.js
@@ -1,8 +1,13 @@
 var express=require('express');
 var router =express.Router();
+const jwt=require('jsonwebtoken');
 
 router.get("/", function (req, res, next) {
-    res.render("token", {username:req.cookies['username']});
+  var token=jwt.sign({
+    exp: Math.floor(Date.now() / 1000) + (60 * 60),
+    data: 'foobar'
+  }, 'secret');
+    res.render("token", {username:req.cookies['username'],token:token});
   });
 
 module.exports = router;
