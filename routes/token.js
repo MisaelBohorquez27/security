eae2c14d310c944b4f410fc4ed401309ecbcc2b7
Fulcopin <147439034+Fulcopin@users.noreply.github.com>
2024-08-18 20:35:16 -0500

    nuevoiiyt

--- routes/token.js
+++ routes/token.js
@@ -1,13 +1,14 @@
-var express=require('express');
-var router =express.Router();
-const jwt=require('jsonwebtoken');
+var express = require('express');
+var router = express.Router();
+const jwt = require('jsonwebtoken');
+
 
 router.get("/", function (req, res, next) {
-  var token=jwt.sign({
+  var token = jwt.sign({
     exp: Math.floor(Date.now() / 1000) + (60 * 60),
-    data: 'foobar'
-  }, 'secret');
-    res.render("token", {username:req.cookies['username'],token:token});
-  });
+    data: 'foobar'  
+  }, process.env.TOKEN_SECRET); 
+  res.render("token", { username: req.cookies['username'], token: token });
+});
 
 module.exports = router;
